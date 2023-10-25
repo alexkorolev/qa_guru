87df9901da6cb1b9aa3d389f444dd72c8af60b09
alexkorolev <alexkorolev86@gmail.com>
2023-10-25 23:18:47 +0300

    Update Main.java

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -1,7 +1,11 @@
 package qa.guru;
 
 public class Main {
+
+    private static String msg = "Hello qa guru!!!!;
+
     public static void main(String[] args){
-        System.out.print("Hello qa guru");
+        System.out.print(msg);
     }
+
 }
