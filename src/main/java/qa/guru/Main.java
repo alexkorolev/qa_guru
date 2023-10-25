2aa3cba34c373a1fd76084ec2bad77c99ed2078b
Aleksei Korolev <alexkorolev86@gmail.com>
2023-10-25 23:46:46 +0300

    hello world changed again

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -2,7 +2,7 @@ package qa.guru;
 
 public class Main {
 
-    private static String msg = "Hello qa guru!!!";
+    private static String msg = "Hello qa guru home work!";
 
     public static void main(String[] args){
         System.out.print(getMessage());
